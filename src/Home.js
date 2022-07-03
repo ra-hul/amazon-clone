8046d5b3e14349b1ceca41bc2813051e36e21368
ra-hul <rahulshanto008@gmail.com>
2022-07-04 04:23:40 +0600

    product components added

--- src/Home.js
+++ src/Home.js
@@ -18,33 +18,41 @@ function Home() {
      image="https://m.media-amazon.com/images/I/61IxdporMdL._AC_UY327_FMwebp_QL65_.jpg"/>
    <Product 
      id="12345"
-     title="The Lean Startup: How SENZER Gaming Headset with Microphone - 7.1 Surround Sound, Lightweight and Ultra-Soft Memory Foam Cushion"
-     price={11.96}
-     rating={5}
-     image="https://m.media-amazon.com/images/I/61IxdporMdL._AC_UY327_FMwebp_QL65_.jpg"/>
+     title="HP DeskJet 2755e Wireless Color All-in-One Printer with bonus 6 months Instant Ink with HP+ (26K67A)"
+     price={10.96}
+     rating={4}
+     image="https://m.media-amazon.com/images/I/61UdeL7aO-L._AC_SX466_.jpg"/>
    
    </div>
    <div className='home__row'>
    <Product 
-     id="12345"
-     title="The Lean Startup: How SENZER Gaming Headset with Microphone - 7.1 Surround Sound, Lightweight and Ultra-Soft Memory Foam Cushion"
-     price={11.96}
+     id="23665"
+     title=" Generic Breathable Nylon Mesh Dog Harness Adjustable Dog Harness and Leash Set Dog Accessories Pet Supplies (Color : D, Size : Small)"
+     price={21.96}
      rating={5}
-     image="https://m.media-amazon.com/images/I/61IxdporMdL._AC_UY327_FMwebp_QL65_.jpg"/>
+     image="https://m.media-amazon.com/images/I/61BTGnZgobL._AC_SX679_.jpg"/>
    <Product 
      id="12345"
-     title="The Lean Startup: How SENZER Gaming Headset with Microphone - 7.1 Surround Sound, Lightweight and Ultra-Soft Memory Foam Cushion"
-     price={11.96}
-     rating={5}
-     image="https://m.media-amazon.com/images/I/61IxdporMdL._AC_UY327_FMwebp_QL65_.jpg"/>
+     title="Cubii JR2/JR2+er Desk Elliptical, Pedal Ex Adjustable Resistance,pment for Home Workouts, Newest Model"
+     price={50.96}
+     rating={3}
+     image="https://m.media-amazon.com/images/I/418HZfOBuTL._AC_SX679_.jpg"/>
 <Product 
-     id="12345"
-     title="The Lean Startup: How SENZER Gaming Headset with Microphone - 7.1 Surround Sound, Lightweight and Ultra-Soft Memory Foam Cushion"
-     price={11.96}
+     id="85236"
+     title="Hook, Line, and Sinker: A Novel Kindle Edition"
+     price={51.96}
      rating={5}
-     image="https://m.media-amazon.com/images/I/61IxdporMdL._AC_UY327_FMwebp_QL65_.jpg"/>
+     image="https://m.media-amazon.com/images/I/41qzpFOaZ2L.jpg"/>
 
 
+   </div>
+   <div className='home__row'>
+   <Product 
+     id="78965"
+     title="Apple Watch Series 3 [GPS 38mm] Smart Watch w/ Silver Aluminum Case  White Sport Band. Fitness  Activity Tracker, Heart Rate Monitor, Retina Display, Water Resistant"
+     price={88.96}
+     rating={5}
+     image="https://m.media-amazon.com/images/I/71vCuRn4CkL._AC_SX522_.jpg"/>
    </div>
 </div>
   
